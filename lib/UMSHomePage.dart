957bebd4aac69d70167e2bd545ce4a53d83dd9b4
shruti248 <shrutiunadkat5@gmail.com>
2024-01-16 10:06:16 +0530

    Display Users data in home page

--- lib/UMSHomePage.dart
+++ lib/UMSHomePage.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,42 @@ class UMSHomePage extends StatefulWidget {
 }
 
 class _UMSHomePageState extends State<UMSHomePage> {
+
+  late Future<Map<String, dynamic>> users;
+  var jsonList;
+
+  void getUsers()  async {
+      try{
+        final dio = Dio();
+        dio.options.headers['Access-Control-Allow-Origin'] = '*';
+
+        final res = await dio.get('http://localhost:3000/api/v1/users');
+
+        if (res.statusCode == 200) {
+          final users = res.data;
+          // print(data['count']);
+          // return data;
+
+          setState(() {
+            jsonList = users['data'] as List;
+          });
+        } else {
+          throw 'Failed to get Users Data. Status code: ${res.statusCode}';
+        }
+
+      }catch(err){
+        print('Try Again ! Error getting User Info. : $err');
+        throw err.toString();
+      }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUsers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,43 +53,41 @@ class _UMSHomePageState extends State<UMSHomePage> {
           title: const Text('Users')
         ),
 
-      body:  Center(
-        child: ListView(
-          // scrollDirection: Axis.horizontal,
-          // reverse: true,
-          children:const [
-             Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text('One' , style: TextStyle(
-                fontSize: 21,
-                fontWeight: FontWeight.w500,
-              ),),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text('Two' , style: TextStyle(
-                fontSize: 21,
-                fontWeight: FontWeight.w500,
-              ),),
-            ),
-            Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Text('Three' , style: TextStyle(
-                fontSize: 21,
-                fontWeight: FontWeight.w500,
-              ),),
-            ),
-            Padding(
-              padding:EdgeInsets.all(8.0),
-              child: Text('Four' , style: TextStyle(
-                fontSize: 21,
-                fontWeight: FontWeight.w500,
-              ),),
-            )
+      body: ListView.builder(
 
+          itemCount : jsonList == null?0 :jsonList.length,
+          itemBuilder: (BuildContext context , int index){
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              elevation: 5,
+              child: ListTile(
+                leading: ClipRect(
+                  child: jsonList[index]['profilePic'] != null
+                      ? Image.network(
+                    jsonList[index]['profilePic'],
+                    width: 50.0, // Adjust the width as needed
+                    height: 50.0, // Adjust the height as needed
+                    fit: BoxFit.cover,
+                  )
+                      : Image.network(
+                    '',
+                    width: 50.0, // Adjust the width as needed
+                    height: 50.0, // Adjust the height as needed
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+
+                title: Text(jsonList[index]['firstName'] + ' ' + jsonList[index]['lastName']),
+                subtitle: Text(jsonList[index]['email']),
+              )
+            )
           ],
-        ),
-      ),
-      );
+        );
+      })
+    
+    );
   }
 }
