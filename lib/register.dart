70235591db5e34e300c93c29bb5ba6fe477ad4b3
shruti248 <shrutiunadkat5@gmail.com>
2024-01-17 12:36:24 +0530

    Profile pic uploading done from register form

--- lib/register.dart
+++ lib/register.dart
@@ -1,21 +1,21 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:typed_data';
 
 class MyForm extends StatefulWidget {
-  const MyForm({super.key});
+  const MyForm({Key? key});
 
   @override
   State<MyForm> createState() => _MyFormState();
 }
 
 class _MyFormState extends State<MyForm> {
-  var _myFormKey=GlobalKey<FormState>();
+  var _myFormKey = GlobalKey<FormState>();
   GlobalKey<ScaffoldMessengerState> _scaffoldKey = GlobalKey<ScaffoldMessengerState>();
-  // var _password = TextEditingController();
-  // var _confirmPassword = TextEditingController();
 
-  // Declare controllers for each form field
   var _firstNameController = TextEditingController();
   var _lastNameController = TextEditingController();
   var _emailController = TextEditingController();
@@ -24,64 +24,74 @@ class _MyFormState extends State<MyForm> {
   var _contactNumberController = TextEditingController();
 
   late Future<Map<String, dynamic>> newUser;
+  Uint8List? profilePicFile;
 
-  // Future<Map<String, dynamic>> userRegistration() async {
-  //   try {
-  //     final dio = Dio();
-  //     dio.options.headers['Access-Control-Allow-Origin'] = '*';
-  //
-  //     final res = await dio.post(
-  //       'http://localhost:3000/api/v1/register',
-  //       data:         {
-  //         "firstName": "abc",
-  //         "lastName": "xyz",
-  //         "email": "t@gmail.com",
-  //         "password": "hahuihuiygvb",
-  //         "profilePic": "",
-  //         "contactNumber": "9086541234",
-  //       },
-  //     );
-  //
-  //     if (res.statusCode == 201) {
-  //       final data = res.data; // No need to use jsonDecode since Dio does it automatically
-  //       print(data);
-  //       return data;
-  //     } else {
-  //       throw "Failed to register. Status code: ${res.statusCode}";
-  //     }
-  //   } catch (err) {
-  //     print('Error during registration: $err');
-  //     throw err.toString();
-  //   }
-  // }
+  Future<Uint8List?> pickFile() async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['jpg', 'jpeg', 'png'],
+      );
+
+
+      if (result != null) {
+        PlatformFile file = result.files.first;
+
+        // Access file content as Uint8List
+        Uint8List fileBytes = file.bytes!;
+        return fileBytes;
+
+      } else {
+        return null;      }
+    } catch (e) {
+      print('Error picking file: $e');
+      return null;
+
+    }
+  }
+
 
   Future<Map<String, dynamic>> userRegistration({
     required String firstName,
     required String lastName,
     required String email,
     required String password,
-    required String profilePic,
+    required Uint8List? profilePic,
     required String contactNumber,
   }) async {
     try {
       final dio = Dio();
       dio.options.headers['Access-Control-Allow-Origin'] = '*';
 
+      FormData formData = FormData();
+
+      formData.fields.addAll([
+        MapEntry('firstName', firstName),
+        MapEntry('lastName', lastName),
+        MapEntry('email', email),
+        MapEntry('password', password),
+        MapEntry('contactNumber', contactNumber),
+      ]);
+
+      if (profilePic != null) {
+        // Convert Uint8List to MultipartFile
+        formData.files.add(MapEntry(
+          'profilePic',
+          MultipartFile.fromBytes(profilePic, filename: 'profilePic.jpg'),
+        ));
+      }
+
+      print('form data ');
+      print(formData);
+
       final res = await dio.post(
         'http://localhost:3000/api/v1/register',
-        data: {
-          'firstName': firstName,
-          'lastName': lastName,
-          'email': email,
-          'password': password,
-          'profilePic': '',
-          'contactNumber': contactNumber,
-        },
+        data: formData,
       );
 
       if (res.statusCode == 200) {
-        final data = res.data; // No need to use jsonDecode since Dio does it automatically
-        print(data);
+        final data = res.data;
+        // print(data);
         return data;
       } else {
         throw 'Failed to register. Status code: ${res.statusCode}';
@@ -92,162 +102,154 @@ class _MyFormState extends State<MyForm> {
     }
   }
 
-  // void initState() {
-  //   super.initState();
-  //   newUser = userRegistration(firstName: _firstNameController.value.text , lastName: _lastNameController.value.text , email: _emailController.value.text , password: _passwordController.value.text , profilePic : "" , contactNumber: _contactNumberController.value.text);
-  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('REGISTER' ,
-        style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-      ),centerTitle: true,
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 25.0 , horizontal: 25.0),
-        child: Form(
+        appBar: AppBar(
+          title: const Text(
+            'REGISTER',
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: const EdgeInsets.symmetric(vertical: 25.0, horizontal: 25.0),
+          child: Form(
             key: _myFormKey,
-            child:Column(
+            child: Column(
               children: <Widget>[
-
                 Row(
                   children: [
-                    // Firstname
-                    Expanded(child:
-                    TextFormField(
-                      controller: _firstNameController,
-                      validator: (String? firstName){
-                        if(firstName == null || firstName.isEmpty){
-                          return "Required";
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
+                    Expanded(
+                      child: TextFormField(
+                        controller: _firstNameController,
+                        validator: (String? firstName) {
+                          if (firstName == null || firstName.isEmpty) {
+                            return "Required";
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
                           labelText: "First Name",
                           hintText: "Enter your First Name",
                           border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(15.0)
-                          )
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
                       ),
                     ),
-                    ),
-
                     SizedBox(width: 10),
-
-                    // last name
-                    Expanded(child:
-                    TextFormField(
-                      controller: _lastNameController,
-                      validator: (String? lastName){
-                        if(lastName == null || lastName.isEmpty){
-                          return "Required";
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
+                    Expanded(
+                      child: TextFormField(
+                        controller: _lastNameController,
+                        validator: (String? lastName) {
+                          if (lastName == null || lastName.isEmpty) {
+                            return "Required";
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
                           labelText: "Last Name",
                           hintText: "Enter your Last Name",
                           border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(15.0)
-                          )
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
                       ),
                     ),
-                    )
-
                   ],
                 ),
-
                 const SizedBox(height: 30),
-
-                // email
                 TextFormField(
                   controller: _emailController,
-                  validator: (String? email){
-                    if(email == null || email.isEmpty){
+                  validator: (String? email) {
+                    if (email == null || email.isEmpty) {
                       return "Required";
                     }
                     return null;
                   },
                   decoration: InputDecoration(
-                      labelText: "Email",
-                      hintText: "Enter your email",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0)
-                      )
+                    labelText: "Email",
+                    hintText: "Enter your email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
                   ),
                 ),
-
                 const SizedBox(height: 30),
-
-                // password
                 TextFormField(
                   controller: _passwordController,
-                  validator: (String? password){
-                    if(password == null || password.isEmpty){
+                  validator: (String? password) {
+                    if (password == null || password.isEmpty) {
                       return "Required";
                     }
-                    if(password.length<6 || password.length>30){
+                    if (password.length < 6 || password.length > 30) {
                       return "Password must be 6 - 30 characters long";
                     }
                     return null;
                   },
-                  // Hidden text
                   obscureText: true,
                   decoration: InputDecoration(
-                      labelText: "Password",
-                      hintText: "Enter your password",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0)
-                      )
+                    labelText: "Password",
+                    hintText: "Enter your password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
                   ),
                 ),
-
                 const SizedBox(height: 30),
-
-                // confirm password
                 TextFormField(
                   controller: _confirmPasswordController,
-                  validator: (String? confirmpassword){
-                    if(confirmpassword == null || confirmpassword.isEmpty){
+                  validator: (String? confirmPassword) {
+                    if (confirmPassword == null || confirmPassword.isEmpty) {
                       return "Required";
                     }
 
-                    // Checking the values of both the entered password & the confirm password
-                    // Required : Controller for that case
-
-                    if(confirmpassword != _passwordController.value.text){
+                    if (confirmPassword != _passwordController.value.text) {
                       return "Passwords do not match.";
                     }
 
                     return null;
                   },
-                  // Hidden text
                   obscureText: true,
                   decoration: InputDecoration(
-                      labelText: "Confirm Password",
-                      hintText: "Enter your password again",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0)
-                      )
+                    labelText: "Confirm Password",
+                    hintText: "Enter your password again",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
                   ),
                 ),
                 const SizedBox(height: 30),
 
-                // profilePic left
-
-                // contact number
+                // NULLL Over HERE only -----------------------
+                TextButton(
+                  onPressed: () async {
+                    Uint8List? fileBytes = await pickFile();
+                    // print("File : ");
+                    // print(fileBytes);
+                    if (fileBytes != null) {
+                      // You can use fileBytes as needed
+                      setState(() {
+                        // Assuming profilePicFile is of type Uint8List?
+                        profilePicFile = fileBytes;
+                      });
+                      // print("ProfilePicFile : ");
+                      // print(profilePicFile);
+                    }
+                  },
+                  // -------------------------------------------------------
+                  child: Text('Select Profile Picture'),
+                ),
+                const SizedBox(height: 30),
                 TextFormField(
                   controller: _contactNumberController,
-                  // This property does not work for web
-                  // keyboardType: TextInputType.phone,
-
-                  validator: (String? contactNumber){
-                    if(contactNumber == null || contactNumber.isEmpty){
+                  validator: (String? contactNumber) {
+                    if (contactNumber == null || contactNumber.isEmpty) {
                       return "Required";
                     }
 
-                    // Adding regular Expression for keyboard type
                     RegExp indianMobileNumber = RegExp(r'^[6-9]\d{9}$');
 
                     if (!indianMobileNumber.hasMatch(contactNumber)) {
@@ -257,57 +259,52 @@ class _MyFormState extends State<MyForm> {
                     return null;
                   },
                   decoration: InputDecoration(
-                      labelText: "Contact Number",
-                      hintText: "Enter your Contact Number",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0)
-                      )
+                    labelText: "Contact Number",
+                    hintText: "Enter your Contact Number",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
                   ),
                 )
               ],
-            )
+            ),
+          ),
         ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
+        floatingActionButton: FloatingActionButton(
         child: Icon(Icons.done),
-        onPressed: () async {
-          if (_myFormKey.currentState?.validate() ?? false) {
-            // If the form is valid, retrieve form data
-            final firstName = _firstNameController.text;
-            final lastName = _lastNameController.text;
-            final email = _emailController.text;
-            final password = _passwordController.text;
-            final contactNumber = _contactNumberController.text;
+    onPressed: () async {
+    if (_myFormKey.currentState?.validate() ?? false) {
+    final firstName = _firstNameController.text;
+    final lastName = _lastNameController.text;
+    final email = _emailController.text;
+    final password = _passwordController.text;
+    final contactNumber = _contactNumberController.text;
 
-            // Call userRegistration method with form data
-            try {
-              await userRegistration(
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                password: password,
-                profilePic: "",
-                contactNumber: contactNumber,
-              );
+    try {
+      await userRegistration(
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        profilePic: profilePicFile,
 
-              // Display success message using a SnackBar
-              _scaffoldKey.currentState?.showSnackBar(
-                SnackBar(
-                  content: Text('Registration successful!'),
-                ),
-              );
+        contactNumber: contactNumber,
+      );
 
-              // Do something after successful registration if needed
-              print('Registration successful!');
-            } catch (error) {
-              // Handle registration error
-              print('Error during registration: $error');
-            }
-          }
-        },
-      ),
+      _scaffoldKey.currentState?.showSnackBar(
+        SnackBar(
+          content: Text('Registration successful!'),
+        ),
+      );
 
+      print('Registration successful!');
+    } catch (error) {
+      print('Error during registration: $error');
+    }
+    }
+    },
+        ),
     );
   }
 }
+
